aa9024f431aca29995e86547e1892ee6353116d2
Drew De Ponte <cyphactor@gmail.com>
2022-02-15 22:33:15 -0500

    Make git::git_current_branch() return full name
    I did this because having the fully qualified name, e.g. refs/heads/main
    is generally more directly usable than the shorthand name, e.g. main
    without having to doing extra lookups.

--- src/ps/git.rs
+++ src/ps/git.rs
@@ -72,7 +72,7 @@ pub fn get_revs<'a>(repo: &'a git2::Repository, start: git2::Oid, end: git2::Oid
 pub fn get_current_branch<'a>(repo: &'a git2::Repository) -> Option<String> {
   // https://stackoverflow.com/questions/12132862/how-do-i-get-the-name-of-the-current-branch-in-libgit2
   match repo.head() {
-    Ok(head_ref) => return head_ref.shorthand().map(String::from), 
+    Ok(head_ref) => return head_ref.name().map(String::from),
     Err(_) => return None
   }
 }
