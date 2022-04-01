3bdc80213f1b31fc4024a70152ed3675ab74bb5e
Drew De Ponte <cyphactor@gmail.com>
2022-04-01 00:50:17 -0400

    Add ps::pull() function
    I am adding the ps::pull() function to be part of the public interface.
    
    I am doing this so that it can be part of the ps library public
    interface and we can use it through git-ps.
    
    The approach I took was defining a new PullError type to identify the
    possible failure cases and have the ps:pull() -> Result<(), PullError>.

--- src/ps/ps.rs
+++ src/ps/ps.rs
@@ -11,6 +11,8 @@ use git2;
 use super::git;
 use regex::Regex;
 use uuid::Uuid;
+use super::utils;
+use std::io;
 
 pub struct PatchStack<'a> {
     pub head: git2::Reference<'a>,
@@ -140,6 +142,30 @@ pub fn add_ps_id(repo: &git2::Repository, commit_oid: git2::Oid, ps_id: Uuid) ->
   tmp_branch_ref.delete()?;
 
   return Ok(amended_patch_oid);
+}
+
+#[derive(Debug)]
+pub enum PullError {
+  ExitStatus(i32),
+  ExitSignal(i32),
+  IOError(io::Error),
+  Unknown
+}
+
+impl From<utils::ExecuteError> for PullError {
+  fn from(e: utils::ExecuteError) -> Self {
+    match e {
+      utils::ExecuteError::SpawnFailure(io_error) => PullError::IOError(io_error),
+      utils::ExecuteError::Failure(io_error) => PullError::IOError(io_error),
+      utils::ExecuteError::ExitStatus(code) => PullError::ExitStatus(code),
+      utils::ExecuteError::ExitSignal(signal) => PullError::ExitSignal(signal),
+      utils::ExecuteError::ExitMissingSignal => PullError::Unknown
+    }
+  }
+}
+
+pub fn pull() -> Result<(), PullError> {
+  utils::execute("git", &["pull", "--rebase"]).map_err(PullError::from)
 }
 
 #[cfg(test)]
