d8d7c0a7b1305a87ae6a6a54642a692ecde21709
Drew De Ponte <cyphactor@gmail.com>
2022-04-02 20:13:21 -0400

    Clean up ps/branch func a bit
    I did this just to make it easier to read and see what was going on. I
    looked at extracting things into specific functions but decided against
    it as nothing seemed like there was clean boundaries without creating
    duplication of work.
    
    My approach was really just removing some useless comments and shifting
    some lines around those that things would be more logically grouped
    together.

--- src/ps/branch.rs
+++ src/ps/branch.rs
@@ -66,18 +66,15 @@ pub fn branch(patch_index: usize) -> Result<(), BranchError>  {
   let patch_stack = ps::get_patch_stack(&repo)?;
   let patch_stack_base_commit = patch_stack.base.peel_to_commit().map_err(|_| BranchError::PatchStackBaseNotFound)?;
   let patches_vec = ps::get_patch_list(&repo, patch_stack);
-
   let patch_oid = patches_vec.get(patch_index).ok_or(BranchError::PatchIndexNotFound)?.oid;
-
   let patch_commit = repo.find_commit(patch_oid).map_err(|_| BranchError::PatchCommitNotFound)?;
+
   let patch_message = patch_commit.message().ok_or(BranchError::PatchMessageMissing)?;
 
-  // - if patch doesn't have patch id, add one
   let new_patch_oid: git2::Oid;
   if let Some(ps_id) = ps::extract_ps_id(patch_message) {
     new_patch_oid = patch_oid;
   } else {
-    // add patch stack id to the commit
     new_patch_oid = ps::add_ps_id(&repo, patch_oid, Uuid::new_v4())?;
   }
 
@@ -88,10 +85,8 @@ pub fn branch(patch_index: usize) -> Result<(), BranchError>  {
   
   let branch_ref_name = branch.get().name().ok_or(BranchError::RrBranchNameNotUtf8)?;
 
-  // TODO: finish handling errors below this
-
   // - cherry pick the patch onto new rr branch
-  let cherry_picked_patch_oid = git::cherry_pick_no_working_copy(&repo, new_patch_oid, branch_ref_name).map_err(BranchError::CherryPickFailed)?;
+  git::cherry_pick_no_working_copy(&repo, new_patch_oid, branch_ref_name).map_err(BranchError::CherryPickFailed)?;
 
   Ok(())
 }
