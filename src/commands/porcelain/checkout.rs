181295c2fdbb90b9ee590034fac2642bc0422462
Drew De Ponte <cyphactor@gmail.com>
2022-04-08 00:36:09 -0400

    Add ps/commands/porcelain/checkout module
    I am adding the checkout module and checkout() function to the
    ps/commands/porcelain module so that I can use it to implement the
    checkout subcommand in the command line interface.

---
+++ src/commands/porcelain/checkout.rs
@@ -0,0 +1,10 @@
+use gps as ps;
+
+pub fn checkout(patch_index: usize) {
+  let repo = ps::plumbing::git::create_cwd_repo().unwrap();
+  let res = ps::checkout(&repo, patch_index);
+  match res {
+    Ok(_) => return,
+    Err(e) => eprintln!("Error: {:?}", e)
+  }
+}
--- src/commands/porcelain/mod.rs
+++ src/commands/porcelain/mod.rs
@@ -9,3 +9,4 @@ pub mod ls;
 pub mod rebase;
 pub mod integrate;
 pub mod show;
+pub mod checkout;
