3a22b9d31e42f3878a281372d6e17177424f9e1d
Drew De Ponte <cyphactor@gmail.com>
2022-04-11 23:40:49 -0400

    Remove an unused file
    Remove an unused file because it isn't being used.

--- src/ps/private/isolate_state_management.rs
+++
@@ -1,6 +0,0 @@
-use super::string_file_io;
-use std::result::Result;
-use std::path::Path;
-use std::path::PathBuf;
-
-
--- src/ps/private/mod.rs
+++ src/ps/private/mod.rs
@@ -5,4 +5,3 @@ pub mod utils;
 pub mod branch;
 pub mod checkout;
 pub mod string_file_io;
-pub mod isolate_state_management;
