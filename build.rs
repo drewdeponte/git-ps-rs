ddfbe19f3b7f2d0467277417632c8885c7131ba9
Drew De Ponte <cyphactor@gmail.com>
2022-04-22 14:07:45 -0400

    Use the Cargo OUT_DIR for shell completions
    Change the output direction of the shell completions to the Cargo
    OUT_DIR so that we are following the conventions.

--- build.rs
+++ build.rs
@@ -1,8 +1,26 @@
 use structopt::clap::Shell;
+use std::env;
+use std::fs;
+use std::process;
 
 include!("src/cli.rs");
 
 fn main() {
-  ApplicationArguments::clap().gen_completions(env!("CARGO_PKG_NAME"), Shell::Zsh, "target");
-  ApplicationArguments::clap().gen_completions(env!("CARGO_PKG_NAME"), Shell::Bash, "target");
+  // OUT_DIR is set by Cargo and it's where any additional build artifacts
+  // are written.
+  let outdir = match env::var_os("OUT_DIR") {
+    Some(outdir) => outdir,
+    None => {
+      eprintln!(
+        "OUT_DIR environment variable not defined. \
+        Please file a bug: \
+        https://github.com/uptech/git-ps-rs/issues/new"
+      );
+      process::exit(1);
+    }
+  };
+  fs::create_dir_all(&outdir).unwrap();
+
+  ApplicationArguments::clap().gen_completions(env!("CARGO_PKG_NAME"), Shell::Zsh, &outdir);
+  ApplicationArguments::clap().gen_completions(env!("CARGO_PKG_NAME"), Shell::Bash, &outdir);
 }
