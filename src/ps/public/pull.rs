176e7f595fdca2d65cb2f0ad7dfc03a9cafa2022
Drew De Ponte <cyphactor@gmail.com>
2022-04-22 22:17:21 -0400

    Fix pull so it doesn't lose commits incorrectly
    Fix the pull command so that it no longer incorrectly loses commits in
    the process of rebasing.
    
    Turned out that it was the `git pull --rebase` that we were executing
    that was actually losing the commits. I dug through a bunch of docs and
    things trying to figure out a way to correct it's behavior but there
    didn't seem to be anything. Therefore I changed the implementation of
    the pull command to do a fetch and then do a rebase, example
    `git rebase --onto origin/main origin/main main`, which seems to have
    the correct behavior.
    
    This resolves issue #43.
    
    [changelog]
    fixed: bug where pull command was losing commits

--- src/ps/public/pull.rs
+++ src/ps/public/pull.rs
@@ -1,4 +1,5 @@
 use super::super::private::utils;
+use super::super::private::git;
 use std::io;
 
 #[derive(Debug)]
@@ -6,6 +7,10 @@ pub enum PullError {
   ExitStatus(i32),
   ExitSignal(i32),
   IOError(io::Error),
+  RepositoryMissing,
+  GetHeadBranchNameFailed,
+  GetUpstreamBranchNameFailed,
+  RebaseFailed(utils::ExecuteError),
   Unknown
 }
 
@@ -22,5 +27,15 @@ impl From<utils::ExecuteError> for PullError {
 }
 
 pub fn pull() -> Result<(), PullError> {
-  utils::execute("git", &["pull", "--rebase"]).map_err(PullError::from)
+  let repo = git::create_cwd_repo().map_err(|_| PullError::RepositoryMissing)?;
+
+  let head_ref = repo.head().map_err(|_| PullError::GetHeadBranchNameFailed)?;
+  let head_branch_shorthand = head_ref.shorthand().ok_or(PullError::GetHeadBranchNameFailed)?;
+  let head_branch_name = head_ref.name().ok_or(PullError::GetHeadBranchNameFailed)?;
+
+  let upstream_branch_name = git::branch_upstream_name(&repo, head_branch_name).map_err(|_| PullError::GetUpstreamBranchNameFailed)?;
+
+  utils::execute("git", &["fetch"]).map_err(PullError::from)?;
+
+  utils::execute("git", &["rebase", "--onto", upstream_branch_name.as_str(), upstream_branch_name.as_str(), head_branch_shorthand]).map_err(PullError::RebaseFailed)
 }
