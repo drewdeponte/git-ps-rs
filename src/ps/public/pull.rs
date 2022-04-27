8affc78a15deb0111b912a9a565acc7464324adf
Drew De Ponte <cyphactor@gmail.com>
2022-04-27 16:44:49 -0400

    Use git:ext_fetch() in pull command
    Use the git::ext_fetch() function in the pull command to eliminate
    duplication of code.
    
    ps-id: f0c47366-7a8c-4f92-9d4a-1cac9bc55dea

--- src/ps/public/pull.rs
+++ src/ps/public/pull.rs
@@ -1,29 +1,13 @@
 use super::super::private::utils;
 use super::super::private::git;
-use std::io;
 
 #[derive(Debug)]
 pub enum PullError {
-  ExitStatus(i32),
-  ExitSignal(i32),
-  IOError(io::Error),
   RepositoryMissing,
   GetHeadBranchNameFailed,
   GetUpstreamBranchNameFailed,
   RebaseFailed(utils::ExecuteError),
-  Unknown
-}
-
-impl From<utils::ExecuteError> for PullError {
-  fn from(e: utils::ExecuteError) -> Self {
-    match e {
-      utils::ExecuteError::SpawnFailure(io_error) => PullError::IOError(io_error),
-      utils::ExecuteError::Failure(io_error) => PullError::IOError(io_error),
-      utils::ExecuteError::ExitStatus(code) => PullError::ExitStatus(code),
-      utils::ExecuteError::ExitSignal(signal) => PullError::ExitSignal(signal),
-      utils::ExecuteError::ExitMissingSignal => PullError::Unknown
-    }
-  }
+  FetchFailed(git::ExtFetchError),
 }
 
 pub fn pull() -> Result<(), PullError> {
@@ -35,7 +19,7 @@ pub fn pull() -> Result<(), PullError> {
 
   let upstream_branch_name = git::branch_upstream_name(&repo, head_branch_name).map_err(|_| PullError::GetUpstreamBranchNameFailed)?;
 
-  utils::execute("git", &["fetch"]).map_err(PullError::from)?;
+  git::ext_fetch().map_err(PullError::FetchFailed)?;
 
   utils::execute("git", &["rebase", "--onto", upstream_branch_name.as_str(), upstream_branch_name.as_str(), head_branch_shorthand]).map_err(PullError::RebaseFailed)
 }
