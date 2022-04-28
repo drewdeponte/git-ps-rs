19fd7cf22b94dc5e38470568b8d4740c7d24683b
Drew De Ponte <cyphactor@gmail.com>
2022-04-28 10:28:50 -0400

    Remove useless comments in config module
    Remove uselss comments in config module so there is less noise.
    
    ps-id: 421b7ba6-883d-4de1-8812-a22cc1381ba0

--- src/ps/private/config.rs
+++ src/ps/private/config.rs
@@ -27,38 +27,5 @@ pub fn read_config(path: &Path) -> Result<Config, ReadConfigError> {
   Ok(config)
 }
 
-// #[derive(Debug)]
-// pub enum FindConfigError {
-//   PathExpandHomeFailed(home_dir::Error),
-//   NotFile(PathBuf),
-//   NotFound
-// }
-
-// pub fn find_config(repo_root: &str) -> Result<PathBuf, FindConfigError> {
-//   let shared_config_path_string = format!("{}/.git-ps/config.toml", repo_root);
-//   let shared_config_path = Path::new(shared_config_path_string .as_str());
-
-//   let repository_level_config_path_string = format!("{}/.git/git-ps/config.toml", repo_root);
-//   let repository_level_config_path = Path::new(repository_level_config_path_string .as_str());
-
-//   let user_level_config_path_string = "~/.config/git-ps/config.toml".to_string();
-//   let user_level_config_path = Path::new(user_level_config_path_string.as_str()).expand_home().map_err(FindConfigError::PathExpandHomeFailed)?;
-
-//   match path_exists_and_is_file(shared_config_path) {
-//     PathExistsAndIsFile::ExistsAndIsFile => Ok(shared_config_path.to_path_buf()),
-//     PathExistsAndIsFile::ExistsButNotFile=> Err(FindConfigError::NotFile(shared_config_path.to_path_buf())),
-//     PathExistsAndIsFile::DoesNotExist => match path_exists_and_is_file(repository_level_config_path) {
-//       PathExistsAndIsFile::ExistsAndIsFile => Ok(repository_level_config_path.to_path_buf()),
-//       PathExistsAndIsFile::ExistsButNotFile => Err(FindConfigError::NotFile(repository_level_config_path.to_path_buf())),
-//       PathExistsAndIsFile::DoesNotExist => match path_exists_and_is_file(&user_level_config_path) {
-//         PathExistsAndIsFile::ExistsAndIsFile => Ok(user_level_config_path.to_path_buf()),
-//         PathExistsAndIsFile::ExistsButNotFile => Err(FindConfigError::NotFile(user_level_config_path.to_path_buf())),
-//         PathExistsAndIsFile::DoesNotExist => Err(FindConfigError::NotFound)
-//       }
-//     }
-//   }
-// }
-
-
 // read_config(path: Path) -> Result<Config, Error>
 // merge(configA: &Config, configB: &Config) -> Config
