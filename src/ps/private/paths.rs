023ce714cecb57b48fda928a26ddbfd803de78ad
Drew De Ponte <cyphactor@gmail.com>
2022-04-28 00:18:54 -0400

    Add config path helpers to paths module
    Add the config file path helpers to the paths module so that we can
    easily get the respective paths for the various config file locals.
    
    This relates to issue #37.
    
    ps-id: 83c865ab-0e4c-47d1-a2d1-faf6b12c0bbc

--- src/ps/private/paths.rs
+++ src/ps/private/paths.rs
@@ -1,6 +1,7 @@
 use git2;
 use std::path::{Path, PathBuf};
 use is_executable::IsExecutable;
+use home_dir::{self, HomeDirExt};
 
 const PATCH_STATES_RELATIVE_PATH: &str = ".git/GIT-PATCH-STACK-PATCH-STATES-V1.json";
 const ISOLATE_LAST_BRANCH_RELATIVE_PATH: &str = ".git/GIT-PATCH-STACK-ISOLATE-LAST-BRANCH";
@@ -20,6 +21,28 @@ pub fn patch_states_path(repo: &git2::Repository) -> Result<PathBuf, PathsError>
 
 pub fn isolate_last_branch_path(repo: &git2::Repository) -> Result<PathBuf, PathsError> {
   repo_root_path(repo).map(|p| p.join(ISOLATE_LAST_BRANCH_RELATIVE_PATH))
+}
+
+pub fn communal_repository_level_config_path(repo_root: &str) -> PathBuf {
+  let path_string = format!("{}/.git-ps/config.toml", repo_root);
+  Path::new(path_string.as_str()).to_path_buf()
+}
+
+pub fn personal_repository_level_config_path(repo_root: &str) -> PathBuf {
+  let path_string = format!("{}/.git/git-ps/config.toml", repo_root);
+  Path::new(path_string .as_str()).to_path_buf()
+}
+
+#[derive(Debug)]
+pub enum UserLevelConfigPathError {
+  PathExpandHomeFailed(home_dir::Error)
+}
+
+pub fn user_level_config_path() -> Result<PathBuf, UserLevelConfigPathError> {
+  let path_string = "~/.config/git-ps/config.toml".to_string();
+  let path = Path::new(path_string.as_str()).expand_home()
+    .map_err(UserLevelConfigPathError::PathExpandHomeFailed)?;
+  Ok(path)
 }
 
 pub enum PathExistsAndIsExecutable {
