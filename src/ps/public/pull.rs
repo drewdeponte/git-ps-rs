0a054df96793b8641c0da156e98eb28512da6e49
Drew De Ponte <cyphactor@gmail.com>
2022-04-29 01:56:23 -0400

    Add ability to config pull cmd to also list patches
    Add ability to configure the pull command to list patches after it has
    completed fetching and rebasing. This is disabled by default. But it can
    be enabled in any of the layers of config files as follows.
    
      [pull]
      show_list_post_pull = true
    
    The approach I took was simply to read the config and at the end if the
    configuration value is enabled then call the list command.
    
    This relates to issue #8.
    
    [changelog]
    added: configuration to enable listing patches at end of the pull command
    
    ps-id: 58ab021d-bf4a-492f-81a1-da28ae33605f

--- src/ps/public/pull.rs
+++ src/ps/public/pull.rs
@@ -1,5 +1,8 @@
 use super::super::private::utils;
 use super::super::private::git;
+use super::super::private::paths;
+use super::super::private::config;
+use super::super::public::list;
 
 #[derive(Debug)]
 pub enum PullError {
@@ -8,10 +11,18 @@ pub enum PullError {
   GetUpstreamBranchNameFailed,
   RebaseFailed(utils::ExecuteError),
   FetchFailed(git::ExtFetchError),
+  GetRepoRootPathFailed(paths::PathsError),
+  PathNotUtf8,
+  GetConfigFailed(config::GetConfigError),
+  ListFailed(list::ListError)
 }
 
 pub fn pull() -> Result<(), PullError> {
   let repo = git::create_cwd_repo().map_err(|_| PullError::RepositoryMissing)?;
+
+  let repo_root_path = paths::repo_root_path(&repo).map_err(PullError::GetRepoRootPathFailed)?;
+  let repo_root_str = repo_root_path.to_str().ok_or(PullError::PathNotUtf8)?;
+  let config = config::get_config(repo_root_str).map_err(PullError::GetConfigFailed)?;
 
   let head_ref = repo.head().map_err(|_| PullError::GetHeadBranchNameFailed)?;
   let head_branch_shorthand = head_ref.shorthand().ok_or(PullError::GetHeadBranchNameFailed)?;
@@ -21,5 +32,11 @@ pub fn pull() -> Result<(), PullError> {
 
   git::ext_fetch().map_err(PullError::FetchFailed)?;
 
-  utils::execute("git", &["rebase", "--no-reapply-cherry-picks", "--onto", upstream_branch_name.as_str(), upstream_branch_name.as_str(), head_branch_shorthand]).map_err(PullError::RebaseFailed)
+  utils::execute("git", &["rebase", "--no-reapply-cherry-picks", "--onto", upstream_branch_name.as_str(), upstream_branch_name.as_str(), head_branch_shorthand]).map_err(PullError::RebaseFailed)?;
+
+  if config.pull.show_list_post_pull {
+    list::list().map_err(PullError::ListFailed)?
+  }
+
+  Ok(())
 }
