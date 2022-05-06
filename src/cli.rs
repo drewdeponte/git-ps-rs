444a1f1e5f10fe304a13c8fd2ff07b37de548139
Drew De Ponte <cyphactor@gmail.com>
2022-05-06 18:06:27 -0400

    Add detailed help info to the `list` command
    Add detailed help informaiton to the `list` command so that people can
    learn about the different representations of state, what they mean, and
    how they should address them.
    
    This relates to issue #83.
    
    [changelog]
    added: detailed help info to `list` command explaining states
    
    ps-id: ea768809-7485-4e41-a51c-90952dfa8f31

--- src/cli.rs
+++ src/cli.rs
@@ -115,8 +115,56 @@ pub enum Command {
     /// remote
     #[structopt(name = "integrate", alias = "int")]
     Integrate(IntegrateCmdOpts),
+
     /// (ls) - List the stack of patches and their associated state info
-    #[structopt(name = "list", alias = "ls")]
+    #[structopt(name = "list", alias = "ls", long_about=r"
+(ls) - List the stack of patches and their associated state info
+
+The `list` command lists out your stack of patches in a format that exposes
+the patch index on the far left followed by the state information, followed by
+the short sha of the git commit, and finally followed by the patch summary.
+
+[index] [status]    [sha] [summary]
+
+The patch index value is used with other commands, e.g. `gps rr
+<patch-index>`.
+
+The state information is broken down into main states and modifiers. The main
+states are as follows.
+
+b    - local branch has been created with the patch
+s    - the patch has been synced to the remote
+rr   - you have requested review of the patch
+int  - you have integrated the patch into upstream
+
+Each of the states can have any of the following modifiers.
+
++    - the patch in your patch stack has changed since the operation
+!    - the remote patch has changed since the operation
+
+To fully understand this lets look at an example. Let say you see the status
+`rr+!` when you ran the `list` command. This is telling you that the current
+patch is in a state of **requested review**, indicated by the `rr`. It is also
+telling you that since you last requested review of that patch changes have
+been made to it, in your patch stack. This is indicated by the `+` modifier.
+In addition it is telling you that the patch on the remote has also changed
+since you last requested review, indicated by the `!` modifier.
+
+The `+` by itself is a pretty common modifier to see as it is there to simply
+remind you that you have made changes to a patch and need to `sync` or
+`request-review` for that patch again.
+
+The `!` modifier is less common as it only happens when the patch on the
+remote has changed since the last operation. This is generally because someone
+either force pushed up a patch out of band of Git Patch Stack to replace that
+commit or because someone added a commit to the branch that Git Patch Stack
+created and associated to that patch. To resolve `!` modifiers you really need
+to go look at the commits on the remote branch and see if there are any
+changes there you want to keep. If so you should cherry-pick the ones you want
+into your patch stack and squash/fix them up into the logical patch using the
+`rebase` command. Then you should `sync` or `request-review` of that patch
+again to get it back in sync.
+")]
     List,
 
     /// Interactively rebase your stack of patches
