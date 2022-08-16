fc9534fbeeda0a4f62cbe151aacc35f42ccfe5c0
Alon Dahari <alon@uptechstudio.com>
2022-08-16 22:13:18 +0100

    Add list dto to config dto
    Add the list dto to the config dto so we can use it safely in the relevant places in the application.
    
    Relates to #123
    
    ps-id: 941ad3df-1b5c-40f0-a702-38dc5c211786

--- src/ps/private/config/mod.rs
+++ src/ps/private/config/mod.rs
@@ -2,6 +2,7 @@ mod request_review_config_dto;
 mod pull_config_dto;
 mod integrate_config_dto;
 mod fetch_config_dto;
+mod list_config_dto;
 mod config_dto;
 mod read_config;
 mod read_config_or_default;
