2f2f06a110c9b62bfa1d8c7f59408a5eb34d2f1e
Ali Caglayan <alizter@gmail.com>
2023-03-18 21:19:06 +0100

    add isolation hook verification
    Signed-off-by: Ali Caglayan <alizter@gmail.com>

--- src/ps/public/branch.rs
+++ src/ps/public/branch.rs
@@ -2,6 +2,10 @@ use super::super::super::ps;
 use super::super::private::git;
 use git2;
 use std::result::Result;
+use super::super::private::utils;
+use super::super::private::paths;
+use super::super::private::hooks;
+use super::super::private::string_file_io::{write_str_to_file, WriteStrToFileError, ReadStringFromFileError};
 
 #[derive(Debug)]
 pub enum BranchError {
@@ -17,6 +21,15 @@ pub enum BranchError {
   GetUpstreamBranchNameFailed,
   GetRemoteNameFailed,
   GetRemoteBranchNameFailed,
+  GetCurrentBranchFailed,
+  PathNotUtf8,
+  FailedToCheckout(utils::ExecuteError),
+  GetIsolateLastBranchPathFailed(paths::PathsError),
+  StoreLastBranchFailed(WriteStrToFileError),
+  ReadLastBranchFailed(ReadStringFromFileError),
+  GetRepoRootPathFailed(paths::PathsError),
+  HookExecutionFailed(utils::ExecuteError),
+  HookNotFound(hooks::FindHookError),
   ForcePushFailed(git::ExtForcePushError),
   FindCommitFailed(git2::Error),
   GetCommitParentZeroFailed(git2::Error),
@@ -25,6 +38,7 @@ pub enum BranchError {
 }
 
 pub fn branch(
+    color:bool,
     start_patch_index: usize,
     end_patch_index_option: Option<usize>,
     branch_name: Option<String>,
@@ -99,6 +113,63 @@ pub fn branch(
     // cherry-pick the single patch into the new branch
     git::cherry_pick_no_working_copy(&repo, &config, start_patch_oid, branch_ref_name, 0).map_err(BranchError::CherryPickFailed)?;
   }
+
+  // get currently checked out branch name
+  let checked_out_branch = git::get_current_branch_shorthand(&repo).ok_or(BranchError::GetCurrentBranchFailed)?;
+  // write currently checked out branch name to disk
+  let path = paths::isolate_last_branch_path(&repo).map_err(BranchError::GetIsolateLastBranchPathFailed)?;
+  write_str_to_file(checked_out_branch.as_str(), path).map_err(BranchError::StoreLastBranchFailed)?;
+
+  // checkout the branch
+  utils::execute("git", &["checkout", &branch_name]).map_err(BranchError::FailedToCheckout)?;
+
+  let repo_root_path = paths::repo_root_path(&repo).map_err(BranchError::GetRepoRootPathFailed)?;
+  let repo_root_str = repo_root_path.to_str().ok_or(BranchError::PathNotUtf8)?;
+  match hooks::find_hook(repo_root_str, "isolate_post_checkout") {
+    Ok(hook_path) => utils::execute(hook_path.to_str().ok_or(BranchError::PathNotUtf8)?, &[]).map_err(BranchError::HookExecutionFailed)?,
+    Err(hooks::FindHookError::NotFound) => {
+      utils::print_warn(color,
+r#"
+The isolate_post_checkout hook was not found!
+
+This hook is NOT required but it is strongly recommended that you set it
+up. It is executed after the temporary isolation branch has been created,
+the patch cherry-picked into it and the isolation branch checked out.
+
+It is intended to be used to further verify patch isolation by verifying
+that your code bases build succeeds and your test suite passes.
+
+You can effectively have it do whatever you want as it is just a hook.
+An exit status of 0, success, informs gps that the further isolation
+verification was successful. Any non-zero exit status will indicate failure
+and cause gps to abort.
+
+You can find more information and examples of this hook and others at
+the following.
+
+https://book.git-ps.sh/tool/hooks.html
+"#)
+    },
+    Err(hooks::FindHookError::NotExecutable(hook_path)) => {
+      let path_str = hook_path.to_str().unwrap_or("unknow path");
+      let msg = format!(
+r#"
+The isolate_post_checkout hook was found at
+
+{}
+
+but it is NOT executable. Due to this the hook is being skipped. Generally
+this can be corrected with the following.
+
+chmod u+x {}
+"#, path_str, path_str);
+      utils::print_warn(color, &msg);
+    },
+    Err(e) => return Err(BranchError::HookNotFound(e))
+  }
+
+  // checkout the branch
+  utils::execute("git", &["checkout", &checked_out_branch]).map_err(BranchError::FailedToCheckout)?;
 
   // If the user wants to create a remote branch, create it by force pushing
   if create_remote {
