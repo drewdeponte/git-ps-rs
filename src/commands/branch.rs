54ab8f362c1f94ec638bb3fb00fd4bf0f825c917
Ali Caglayan <alizter@gmail.com>
2023-03-18 15:30:21 +0100

    gps branch: auto-generate names when -n not provided
    We now autogenerate names for `gps branch` when the optional `-n`
    argument is not provided. The naming schema is slightly different to
    that of `gps rr` since we don't want to have a conflict. Therefore we
    simply prepend `gps-branch-` to the `rr` name of the first patch.
    
    Signed-off-by: Ali Caglayan <alizter@gmail.com>
    
    <!-- ps-id: dd9a23d4-4fe3-4f5e-9600-e1a2bf9d752c -->

--- src/commands/branch.rs
+++ src/commands/branch.rs
@@ -2,7 +2,7 @@ use gps as ps;
 use std::option::Option;
 use std::string::String;
 
-pub fn branch(start_patch_index: usize, end_patch_index_option: Option<usize>, branch_name: String) {
+pub fn branch(start_patch_index: usize, end_patch_index_option: Option<usize>, branch_name: Option<String>) {
   match ps::branch(start_patch_index, end_patch_index_option, branch_name) {
     Ok(_) => {},
     Err(e) => {
