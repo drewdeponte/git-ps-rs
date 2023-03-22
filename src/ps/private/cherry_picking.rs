caf3d4dd5feedf800188310fa17b3cfd9234e5d0
Drew De Ponte <cyphactor@gmail.com>
2023-03-22 09:05:14 -0400

    Add docs to map_range_for_cherry_pick()
    Add docs to map_range_for_cherry_pick() so that I can reference the
    details of how it works without having to read the code in the future.
    
    <!-- ps-id: d4db785f-443c-4c20-8fc6-57fe9632a4b9 -->

--- src/ps/private/cherry_picking.rs
+++ src/ps/private/cherry_picking.rs
@@ -12,6 +12,24 @@ pub enum MapRangeForCherryPickError {
     EndIndexNotFound,
 }
 
+/// Convert from a `start_patch_index` and an optional `end_patch_index` into a `CherryPickRange`
+/// containing a `root_oid` and optional `leaf_oid` that are ready to be used with the
+/// cherry_pick() function.
+///
+/// Basically this maps either an individual patch index or patch series indexes from the world of
+/// patch stack down to the world of Git so that the cherry_pick() operation can be performed.
+///
+/// The `start_patch_index` specifies either the only patch to include in the cherry pick or the
+/// beginning patch of a patch series to be cherry picked. It is inclusive, meaning the patch
+/// matching the index will be cherry picked.
+///
+/// The `end_patch_index` specifies the patch index to the patch that ends the series of patches
+/// you want to cherry pick. It is inclusive, meaning the patch matching the index will be cherry
+/// picked. If you want to only cherry pick the patch that matches the `start_patch_index` you can
+/// simply set `end_patch_index` to `None`.
+///
+/// Returns a `CherryPickRange` containing a `root_oid` and `leaf_oid` ready to be used with the
+/// cherry_pick() function to perform cherry pick either a single patch or a series of patches.
 pub fn map_range_for_cherry_pick(
     patches_vec: &[ps::ListPatch],
     start_patch_index: usize,
