d1f238d581f00784d6f291a6192e41c39884c3f9
Drew De Ponte <cyphactor@gmail.com>
2023-10-17 10:48:39 -0400

    Change iso command args to patch_index_or_range
    Change iso command args to patch_index_or_range so that it patches our
    new standard command line interface for dealing with a patch or a series
    of patches.
    
    [changelog]
    changed: isolate command args to use new patch index or range interface
    
    <!-- ps-id: aa73bc98-3e9a-4d32-98f6-74353c71f2a7 -->

--- src/commands/isolate.rs
+++ src/commands/isolate.rs
@@ -1,23 +1,55 @@
+use super::patch_index_range::PatchIndexRange;
 use super::utils::print_err;
 use gps as ps;
+use std::str::FromStr;
 
-pub fn isolate(patch_index: Option<usize>, end_patch_index: Option<usize>, color: bool) {
-    match ps::isolate(patch_index, end_patch_index, color) {
-        Ok(_) => {}
-        Err(ps::IsolateError::UncommittedChangesExist) => {
-            print_err(
+pub fn isolate(patch_index_or_range: Option<String>, color: bool) {
+    match patch_index_or_range {
+        Some(pir) => match PatchIndexRange::from_str(&pir) {
+            Ok(patch_index_range) => match ps::isolate(
+                Some(patch_index_range.start_index),
+                patch_index_range.end_index,
                 color,
-                r#"
+            ) {
+                Ok(_) => {}
+                Err(ps::IsolateError::UncommittedChangesExist) => {
+                    print_err(
+                        color,
+                        r#"
   gps isolate requires a clean working directory but it looks like yours is dirty.
 
   It is recommended that you create a WIP commit. But, you could also use git stash if you prefer.
         "#,
-            );
-            std::process::exit(1);
-        }
-        Err(e) => {
-            print_err(color, format!("\nError: {:?}\n", e).as_str());
-            std::process::exit(1);
-        }
+                    );
+                    std::process::exit(1);
+                }
+                Err(e) => {
+                    print_err(color, format!("\nError: {:?}\n", e).as_str());
+                    std::process::exit(1);
+                }
+            },
+            Err(e) => {
+                eprintln!("Error: {:?}", e);
+                std::process::exit(1);
+            }
+        },
+        None => match ps::isolate(None, None, color) {
+            Ok(_) => {}
+            Err(ps::IsolateError::UncommittedChangesExist) => {
+                print_err(
+                    color,
+                    r#"
+  gps isolate requires a clean working directory but it looks like yours is dirty.
+
+  It is recommended that you create a WIP commit. But, you could also use git stash if you prefer.
+        "#,
+                );
+                std::process::exit(1);
+            }
+            Err(e) => {
+                print_err(color, format!("\nError: {:?}\n", e).as_str());
+                std::process::exit(1);
+            }
+        },
     }
 }
