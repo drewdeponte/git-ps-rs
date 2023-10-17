3ce1dfa2d876e02c37a7c4a4e769d929dcb44532
Drew De Ponte <cyphactor@gmail.com>
2023-10-17 10:14:39 -0400

    Add patch series support to sync command
    Add patch series support to sync command so that users would be able to
    sync their patch series.
    
    [changelog]
    added: patch series support to the sync command
    
    <!-- ps-id: 198d2e18-8144-4ef5-99e8-2a1b07a306b6 -->

--- src/commands/request_review_branch.rs
+++ src/commands/request_review_branch.rs
@@ -1,68 +1,8 @@
+use super::patch_index_range::PatchIndexRange;
 use gps as ps;
 use std::option::Option;
 use std::str::FromStr;
 use std::string::String;
-
-#[derive(Debug, Clone)]
-pub struct PatchIndexRange {
-    pub start_index: usize,
-    pub end_index: Option<usize>,
-}
-
-#[derive(Debug)]
-pub enum ParsePatchIndexOrRangeError {
-    InvalidIndexRange(String),
-    UnparsableIndex(String, std::num::ParseIntError),
-    StartPatchIndexLargerThanEnd(String),
-}
-
-impl std::str::FromStr for PatchIndexRange {
-    type Err = ParsePatchIndexOrRangeError;
-
-    /// Parse string representation of an a patch index or patch index range
-    ///
-    /// A patch index style would simply be a string that is the patch index, e.g. "12". A patch index
-    /// range on the other hand is two patch index strings separated by a dash character, e.g. "2-4".
-    /// The left most patch index is the starting patch index and the right most patch index is the
-    /// ending patch index.
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let patch_index_or_range_parts: Vec<&str> = s.split('-').collect();
-        let num_parts = patch_index_or_range_parts.len();
-        if num_parts == 1 {
-            let patch_start_index_str = patch_index_or_range_parts.first().unwrap();
-            let patch_start_index = patch_start_index_str.parse::<usize>().map_err(|e| {
-                ParsePatchIndexOrRangeError::UnparsableIndex(patch_start_index_str.to_string(), e)
-            })?;
-            Ok(PatchIndexRange {
-                start_index: patch_start_index,
-                end_index: None,
-            })
-        } else if num_parts == 2 {
-            let patch_start_index_str = patch_index_or_range_parts.first().unwrap();
-            let patch_end_index_str = patch_index_or_range_parts.get(1).unwrap();
-            let patch_start_index = patch_start_index_str.parse::<usize>().map_err(|e| {
-                ParsePatchIndexOrRangeError::UnparsableIndex(patch_start_index_str.to_string(), e)
-            })?;
-            let patch_end_index = patch_end_index_str.parse::<usize>().map_err(|e| {
-                ParsePatchIndexOrRangeError::UnparsableIndex(patch_end_index_str.to_string(), e)
-            })?;
-            if patch_end_index > patch_start_index {
-                Ok(PatchIndexRange {
-                    start_index: patch_start_index,
-                    end_index: Some(patch_end_index),
-                })
-            } else {
-                Err(ParsePatchIndexOrRangeError::StartPatchIndexLargerThanEnd(
-                    s.to_string(),
-                ))
-            }
-        } else {
-            Err(ParsePatchIndexOrRangeError::InvalidIndexRange(
-                s.to_string(),
-            ))
-        }
-    }
-}
 
 pub fn request_review_branch(patch_index_or_range: String, branch_name: Option<String>) {
     match PatchIndexRange::from_str(&patch_index_or_range) {
--- src/ps/public/request_review.rs
+++ src/ps/public/request_review.rs
@@ -121,7 +121,7 @@ pub fn request_review(
 
     // sync patch up to remote
     let (patch_upstream_branch_name, _patch_upstream_branch_remote_name) =
-        ps::public::sync::sync(patch_index, given_branch_name)
+        ps::public::sync::sync(patch_index, None, given_branch_name)
             .map_err(RequestReviewError::SyncFailed)?;
 
     // execute post sync hook
